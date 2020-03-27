494919fd4f19b12c51691756f93dba38ee37ba31
ReinaA98 <61682113+ReinaA98@users.noreply.github.com>
2020-03-27 13:12:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+var http = require('http');
+var fs = require('fs');
+
+const PORT=3000; 
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
