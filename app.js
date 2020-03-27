bc7e2e9ddaeb38fefbffaeba4accf83406367627
TwanBr <twnbrwrs@gmail.com>
2020-03-27 12:07:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('Under construction');
+  res.end('./simpleGame_v3.html');  
 });
 
 server.listen(port,() => {
