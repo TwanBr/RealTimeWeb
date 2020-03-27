8be6a882b0d4369548118a4c9bb3a251bcedc98c
TwanBr <twnbrwrs@gmail.com>
2020-03-27 13:09:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const http = require('http');
-const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  return res.redirect('./index.html');
-  //res.end('Dear lord WHY');  
-});
-
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
