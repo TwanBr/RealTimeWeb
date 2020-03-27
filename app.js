7cf848b410c41c5c3a36455d992059af82553dcb
TwanBr <twnbrwrs@gmail.com>
2020-03-27 13:18:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var http = require('http');
+var static = require('node-static');
+var file = new static.Server();
 var fs = require('fs');
 
-const PORT=3000; 
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
+fs.readFile('./index.html',function(err,html) {
+  if (err) throw err;
+  http.createServer(function(request, response) {
+    request.addListener('end', function() {
+      file.serve(request, response);
+    }).resume();
+    
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(html);  
+    response.end(); 
+  }).listen(process.env.PORT || 3000);
 });
